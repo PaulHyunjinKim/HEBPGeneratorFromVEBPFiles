884d201eeb1b81a9101ef5f58154e5dd427393d7
PaulHyunjinKim <kim.3983@buckeyemail.osu.edu>
2016-12-09 13:50:08 -0500

    Created project for HEBP generator from VEBP

---
+++ HEBPEnumeratorFromVEBP/HEBPEnumeratorFromVEBP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEBPEnumeratorFromVEBP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press enter key to finish...");
+            Console.ReadKey();
+        }
+    }
+}
