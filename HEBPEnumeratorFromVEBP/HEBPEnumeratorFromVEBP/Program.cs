04f9fcabb84f283e31350a675887be564c3fd050
Gunyoung kim <Gunyoung kim@DESKTOP-7K9F2HA>
2016-12-12 13:07:28 -0500

    Finshed to implement HEBP enumeration from VEBP Files

--- HEBPEnumeratorFromVEBP/HEBPEnumeratorFromVEBP/Program.cs
+++ HEBPEnumeratorFromVEBP/HEBPEnumeratorFromVEBP/Program.cs
@@ -14,26 +14,43 @@ namespace HEBPEnumeratorFromVEBP
         public const int N = 5;
         public const int numberOfSameBits = 14;
         public static UInt32 numbOfElements = 0;
+        public static int[] maskArray;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            int fileNumber = 0;
+            GlobalVar.maskArray = new int[GlobalVar.M * (GlobalVar.N - 1)];
+            GlobalVar.maskArray[0] = BitVector32.CreateMask();
+            for (int i = 1; i < GlobalVar.M * (GlobalVar.N - 1); i++)
+            {
+                GlobalVar.maskArray[i] = BitVector32.CreateMask(GlobalVar.maskArray[i - 1]);
+            }
 
             Dictionary<int, Dictionary<int, List<List<int>>>> HEBPiMap = new Dictionary<int, Dictionary<int, List<List<int>>>>();
             using (BinaryReader readFile = new BinaryReader(File.Open("newHEBPiSet_" + GlobalVar.M + ".bin", FileMode.Open)))
             {
                 HEBPiMap = hebpMapFromBinaryFile(readFile);
             }
-            
+
+            int fileNumber = 0;
             using (BinaryReader VEBPFileReader = new BinaryReader(File.Open(GlobalVar.M + "x" + GlobalVar.M + "Files\\" + fileNumber + ".bin", FileMode.Open)))
             {
-                long length = VEBPFileReader.BaseStream.Length;
-                while(VEBPFileReader.BaseStream.Position != length)
+                using (BinaryWriter EBPFileWriter = new BinaryWriter(File.Open(GlobalVar.M + "x" + GlobalVar.M + "Files\\" + fileNumber + "_OUT.bin", FileMode.Create)))
                 {
-                    UInt32 VEBPInt = VEBPFileReader.ReadUInt32();
-                    //HEBPEnumeration
+                    long length = VEBPFileReader.BaseStream.Length;
+                    while (VEBPFileReader.BaseStream.Position != length)
+                    {
+                        try
+                        {
+                            int VEBPInt = Convert.ToInt32(VEBPFileReader.ReadUInt32());
+                            HEBPEnumeration(VEBPInt, ref HEBPiMap, EBPFileWriter);
+                        }
+                        catch (System.OverflowException)
+                        {
+                            Console.WriteLine("Overflow in uint-to-int conversion.");
+                        }
+                    }
                 }
             }
 
@@ -41,12 +58,71 @@ namespace HEBPEnumeratorFromVEBP
             Console.ReadKey();
         }
 
-        static void HEBPEnumeration(UInt32 VEBPInt, ref Dictionary<int, Dictionary<int, List<List<int>>>> HEBPiMap)
+        static void HEBPEnumeration(int VEBPInt, ref Dictionary<int, Dictionary<int, List<List<int>>>> HEBPiMap, BinaryWriter EBPFileWriter)
         {
             //implement HEBPEnumeration with distinctHEBPEnumeration()
+            int InitCCInt = 0;
+            for (int i = 0; i < GlobalVar.N; i++)
+                InitCCInt += i * Convert.ToInt32(Math.Pow(Convert.ToDouble(GlobalVar.N), Convert.ToDouble(i)));//test!
+
+            List<int> VEIntSet = VEIntSetFromVEBPInt(VEBPInt);//test!    
+
+            InitCCInt = HEBPiMap[InitCCInt][VEIntSet[0]][0][1];
+
+            BitVector32 HEBPBits = new BitVector32(0);
+
+            distinctHEBPEnumeration(VEBPInt, ref HEBPBits, 0, ref HEBPiMap, ref VEIntSet, InitCCInt, EBPFileWriter);
+                    
         }
 
+        static void distinctHEBPEnumeration(int VEBPInt, ref BitVector32 HEBPBits, int curCol, ref Dictionary<int, 
+                                            Dictionary<int, List<List<int>>>> HEBPiMap, ref List<int> VEIntSet, int CCInt, BinaryWriter EBPFileWriter)
+        {
+            if(curCol == GlobalVar.M - 1)
+            {
+                BitVector32 VEBPBits = new BitVector32(VEBPInt);
+                Console.Write("VEBPBits: " + VEBPBits + "  ,  " + "HEBPBits: " + HEBPBits);
+            }
+            else
+            {
+                int VEInt = VEIntSet[curCol + 1];
+                foreach( var element in HEBPiMap[CCInt][VEInt])
+                {
+                    int HEint = element[0];
+                    int nextCCint = element[1];
+                    BitVector32 tempHEBits = new BitVector32(HEint);
+                    //make HEBPBits..
+                    for(int i=0; i<GlobalVar.N; i++)
+                    {
+                        int originalIndex = GlobalVar.maskArray[i];
+                        int newIndex = GlobalVar.maskArray[curCol*GlobalVar.N+i];
+                        HEBPBits[newIndex] = tempHEBits[originalIndex];
+                    }
+                    distinctHEBPEnumeration(VEBPInt, ref HEBPBits, curCol + 1, ref HEBPiMap, ref VEIntSet, CCInt, EBPFileWriter);
+                }
+            }
 
+        }
+
+        static List<int> VEIntSetFromVEBPInt(int VEBPInt)
+        {
+            List<int> VEIntSet = new List<int>(GlobalVar.M);
+            BitVector32 VEBPBits = new BitVector32(VEBPInt);
+        
+            for(int i=0; i<GlobalVar.M; i++)
+            {
+                BitVector32 tempVEBits = new BitVector32(0);
+                for(int j=0; j<GlobalVar.N-1; j++)
+                {
+                    int originalIndex = GlobalVar.maskArray[j * GlobalVar.M + i];
+                    int newIndex = GlobalVar.maskArray[j];
+                    tempVEBits[newIndex] = VEBPBits[originalIndex];
+                }
+                VEIntSet[i] = tempVEBits.Data;
+            }
+
+            return VEIntSet;
+        }
 
         static Dictionary<int, Dictionary<int, List<List<int>>>> hebpMapFromBinaryFile(BinaryReader readFile)
         {
